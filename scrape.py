08971a278bdcc18f5995be88f033393348fc4ae5
vikrant shukla <94588590+vikrant-shukla@users.noreply.github.com>
2023-08-19 23:02:36 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,7 +7,7 @@ from login import login
 
 driver = openBrowserAndGotoURL('https://www.draftkings.com/lineup/upload')
 
-login(driver, "sonusanglikar54@gmail.com", "Sonu@12345")
+login(driver, "xyz@gmail.com", "xyz@12345")
 
 SelectAndDownloadFiles(driver,"NBA","CLASSIC")
 
